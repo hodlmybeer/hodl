6adad17b738df1bcadc44aebd3d91c63ccc2be86
Anton Cheng <antonassocareer@gmail.com>
2021-05-10 17:54:25 +0800

    update: use hardhat typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(await account.address);
   }
 });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+export default {
   solidity: "0.7.3",
 };
-
--- test/sample-test.js
+++ test/sample-test.js
@@ -2,8 +2,8 @@ const { expect } = require("chai");
 
 describe("Hodl", function() {
   it("Should return the new greeting once it's changed", async function() {
-    const Hodl = await ethers.getContractFactory("Hodl");
-    const greeter = await Greeter.deploy();
+    const HodlShare = await ethers.getContractFactory("HodlShare");
+    const greeter = await HodlShare.deploy();
     
     // await greeter.deployed();
     // expect(await greeter.greet()).to.equal("Hello, world!");
